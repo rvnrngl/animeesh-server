21f0b9007af99883da0bfee6d49ad2738c2869b9
Reevss <0217.raven.ringel@gmail.com>
2023-09-12 17:03:19 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,73 @@
+import express from "express";
+import cors from "cors";
+import { META } from "@consumet/extensions";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
+
+const app = express();
+const port = process.env.PORT;
+const anilist = new META.Anilist();
+
+app.use(cors());
+
+app.use("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+/*--------------------Get Trending Anime-----------------------*/
+app.get("/api/trending", async (req, res) => {
+  try {
+    const data = await anilist.fetchTrendingAnime();
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res
+      .status(500)
+      .json({ error: "Error fetching data from the external API" });
+  }
+});
+
+/*--------------------Get Trending Anime-----------------------*/
+app.get("/api/recent", async (req, res) => {
+  try {
+    const data = await anilist.fetchRecentEpisodes();
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res
+      .status(500)
+      .json({ error: "Error fetching data from the external API" });
+  }
+});
+
+/*------------Get Anime Info and Episode List using anime id param------------*/
+app.get("/api/info", async (req, res) => {
+  const id = req.query.id;
+  try {
+    const response = await anilist.fetchAnimeInfo(id);
+    res.json(response);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res
+      .status(500)
+      .json({ error: "Error fetching data from the external API" });
+  }
+});
+
+/*--------------------Get Episode Url using episode id-----------------------*/
+app.get("/api/episode", async (req, res) => {
+  const episodeId = req.query.id;
+  try {
+    const data = await anilist.fetchEpisodeSources(episodeId);
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res
+      .status(500)
+      .json({ error: "Error fetching data from the external API" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running in port ${port}...`);
+});
