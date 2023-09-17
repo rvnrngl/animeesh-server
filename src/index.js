95bd32aaae13b813e04c374f5451c6d26464aa5f
Reevss <0217.raven.ringel@gmail.com>
2023-09-17 15:22:05 +0800

    added index.html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import axios from "axios";
 import cors from "cors";
+import fs from "fs";
 import { META } from "@consumet/extensions";
 import cron from "node-cron";
 import dotenv from "dotenv";
@@ -14,24 +15,19 @@ const anilist = new META.Anilist();
 
 app.use(cors());
 
-app.get("/keep-alive", (req, res) => {
-  res.send("Successfully");
-});
-
-/*---------------Schedule cron every 14 mins to keep server alive------------------*/
-cron.schedule("*/14 * * * *", () => {
-  axios
-    .get(`${URL}/keep-alive`)
-    .then((response) => {
-      console.log(`${response.data} pinged! Scheduled at ${new Date()}`);
-    })
-    .catch((error) => {
-      console.error(`Error while pinging keep-alive endpoint: ${error}`);
-    });
-});
-
 app.get("/", (req, res) => {
-  res.send("Server running!🎉");
+  // Read the contents of the HTML file
+  fs.readFile("public/index.html", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    // Send the HTML content as the response
+    res.send(data);
+  });
+  // res.send("Server running!🎉");
 });
 
 /*--------------------Get Trending Anime-----------------------*/
@@ -116,6 +112,22 @@ app.get("/api/episode", async (req, res) => {
       .status(500)
       .json({ error: "Error fetching data from the external API" });
   }
+});
+
+/*---------------Schedule cron every 14 mins to keep server alive------------------*/
+cron.schedule("*/14 * * * *", () => {
+  axios
+    .get(`${URL}/keep-alive`)
+    .then((response) => {
+      console.log(`${response.data} pinged! Scheduled at ${new Date()}`);
+    })
+    .catch((error) => {
+      console.error(`Error while pinging keep-alive endpoint: ${error}`);
+    });
+});
+
+app.get("/keep-alive", (req, res) => {
+  res.send("Successfully");
 });
 
 app.listen(PORT, () => {
