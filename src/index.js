a1995f510f5aa47f201769b178c3a2cc2339b454
Reevss <0217.raven.ringel@gmail.com>
2023-10-12 16:56:11 +0800

    placed all /api endpoints to an file route

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import axios from "axios";
 import cors from "cors";
 import fs from "fs";
-import { META } from "@consumet/extensions";
 import cron from "node-cron";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
@@ -10,18 +9,19 @@ dotenv.config({ path: ".env.local" });
 
 import { userRouter } from "./routes/users.js";
 import { animesRouter } from "./routes/animes.js";
+import { apiRouter } from "./routes/api.js";
 
 const PORT = process.env.PORT;
 const URL = process.env.URL;
 const MONGP_DB_PASSWORD = process.env.MONGO_DB_PASSWORD;
 
 const app = express();
-const anilist = new META.Anilist();
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/auth", userRouter);
+app.use("/api", apiRouter);
 app.use("/watch-list", animesRouter);
 
 mongoose.connect(
@@ -29,7 +29,6 @@ mongoose.connect(
 );
 
 app.get("/", (req, res) => {
-  // Read the contents of the HTML file
   fs.readFile("public/index.html", "utf8", (err, data) => {
     if (err) {
       console.error(err);
@@ -38,90 +37,6 @@ app.get("/", (req, res) => {
     }
     res.send(data);
   });
-});
-
-/*--------------------Get Trending Anime-----------------------*/
-app.get("/api/trending", async (req, res) => {
-  try {
-    const data = await anilist.fetchTrendingAnime();
-    res.json(data);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    res
-      .status(500)
-      .json({ error: "Error fetching data from the external API" });
-  }
-});
-
-/*--------------------Get Recent Anime-----------------------*/
-app.get("/api/recent", async (req, res) => {
-  const provider = req.query.provider;
-  const pageNumber = req.query.pageNumber;
-  const itemsPerPage = req.query.itemsPerPage;
-
-  try {
-    const data = await anilist.fetchRecentEpisodes(
-      provider,
-      pageNumber,
-      itemsPerPage
-    );
-    res.json(data);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    console.log("Retrying...");
-    // try another approach if not working
-    try {
-      const data = await anilist.advancedSearch(
-        undefined,
-        "ANIME",
-        pageNumber,
-        itemsPerPage,
-        undefined,
-        ["UPDATED_AT_DESC"],
-        undefined,
-        undefined,
-        undefined,
-        "RELEASING",
-        undefined
-      );
-      data.retry = true;
-      res.json(data);
-      console.log("Retry successfull!");
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      res
-        .status(500)
-        .json({ error: "Error fetching data from the external API" });
-    }
-  }
-});
-
-/*------------Get Anime Info and Episode List using anime id param------------*/
-app.get("/api/info", async (req, res) => {
-  const id = req.query.id;
-  try {
-    const response = await anilist.fetchAnimeInfo(id);
-    res.json(response);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    res
-      .status(500)
-      .json({ error: "Error fetching data from the external API" });
-  }
-});
-
-/*--------------------Get Episode Url using episode id-----------------------*/
-app.get("/api/episode", async (req, res) => {
-  const episodeId = req.query.id;
-  try {
-    const data = await anilist.fetchEpisodeSources(episodeId);
-    res.json(data);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    res
-      .status(500)
-      .json({ error: "Error fetching data from the external API" });
-  }
 });
 
 /*---------------Schedule cron every 14 mins to keep server alive------------------*/
---
+++ src/routes/api.js
@@ -0,0 +1,91 @@
+import express from "express";
+import { META } from "@consumet/extensions";
+
+const anilist = new META.Anilist();
+const router = express.Router();
+
+/*--------------------Get Trending Anime-----------------------*/
+router.get("/trending", async (req, res) => {
+  try {
+    const data = await anilist.fetchTrendingAnime();
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res
+      .status(500)
+      .json({ error: "Error fetching data from the external API" });
+  }
+});
+
+/*--------------------Get Recent Anime-----------------------*/
+router.get("/recent", async (req, res) => {
+  const provider = req.query.provider;
+  const pageNumber = req.query.pageNumber;
+  const itemsPerPage = req.query.itemsPerPage;
+
+  try {
+    const data = await anilist.fetchRecentEpisodes(
+      provider,
+      pageNumber,
+      itemsPerPage
+    );
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    console.log("Retrying...");
+    // try another approach if not working
+    try {
+      const data = await anilist.advancedSearch(
+        undefined,
+        "ANIME",
+        pageNumber,
+        itemsPerPage,
+        undefined,
+        ["UPDATED_AT_DESC"],
+        undefined,
+        undefined,
+        undefined,
+        "RELEASING",
+        undefined
+      );
+      data.retry = true;
+      res.json(data);
+      console.log("Retry successfull!");
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      res
+        .status(500)
+        .json({ error: "Error fetching data from the external API" });
+    }
+  }
+});
+
+/*------------Get Anime Info and Episode List using anime id param------------*/
+router.get("/info", async (req, res) => {
+  const id = req.query.id;
+  try {
+    const response = await anilist.fetchAnimeInfo(id);
+    res.json(response);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res
+      .status(500)
+      .json({ error: "Error fetching data from the external API" });
+  }
+});
+
+/*--------------------Get Episode Url using episode id-----------------------*/
+router.get("/episode", async (req, res) => {
+  const episodeId = req.query.id;
+  try {
+    const data = await anilist.fetchEpisodeSources(episodeId);
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res
+      .status(500)
+      .json({ error: "Error fetching data from the external API" });
+  }
+});
+
+export { router as apiRouter };
